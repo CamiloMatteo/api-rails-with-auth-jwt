7a48c7328784975e1de7209e5fdecb30b3c4133b
Camilo Matteo <matteo.camilo.k@gmail.com>
2020-06-12 11:05:53 -0400

    added json response in controller sessions and registers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: %i[registration sessions passwors]
+  devise_for :users, skip: %i[registration sessions passwords]
   devise_scope :user do
     post '/signup', to: 'registrations#create'
     post '/login', to: 'sessions#create'
